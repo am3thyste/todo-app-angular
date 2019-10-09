ec6b10e0e7c099cbf243389cd28ba715b35e4f99
am3thyste <am3thyste.ursuludmila2010@gmail.com>
2019-10-09 15:08:48 +0200

    project angular basics

--- project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Sushi', 'Rice, fish, avocado, algae', 'https://images.japancentre.com/recipes/pics/18/main/makisushi.jpg?1557308201'),
+    new Recipe('Sushi', 'Rice, fish, avocado, algae', 'https://images.japancentre.com/recipes/pics/18/main/makisushi.jpg?1557308201')
+
+  ];
 
   constructor() { }
 
